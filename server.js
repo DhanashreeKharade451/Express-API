ec2ded4ebffa26d401da6157a3c3604ffa8ed151
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-26 16:19:29 -0400

    Builded Registration endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import User from './models/user';
 
 
 const app = new express();
@@ -14,9 +15,20 @@ const expiration = '2h'
 app.use(express.json());
 
 //register /signup route
-app.post('/api/users/register', async(req, res) => {
+app.post('/api/users/register', async (req, res) => {
 try{
-   
+    const {email} = req.body;
+
+    // Check if user exists
+    const existingUser = await User.findOne({email});
+    if (existingUser){
+        return res.status(400).json({error: 'A user with this email already exists'});
+    }
+     const user = await User.create(req.body);
+     console.log(user);
+     res.status(201).json(user);
+
+
 }catch(error){
 
 }
